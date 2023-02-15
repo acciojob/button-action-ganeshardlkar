46ac80e4ed6636c73b6c6a2d6f42431d2c56ff7c
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-02-15 12:05:58 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ import './../styles/App.css';
 
 const App = (props) => {
 
+  const clickHandler = () => {
+    document.getElementById('para').style.display='block';
+  }
+
   return (
     <div className="App" id="main">
       // Do not alter the main div
       <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-      <button id="click"></button>
+      <button id="click" onClick={clickHandler}>Click me</button>
     </div>
   );
 }
