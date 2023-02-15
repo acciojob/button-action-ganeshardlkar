c2f3b556dfc95a9eb583ca870c3fe93910040066
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-02-15 12:10:58 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const App = (props) => {
   return (
     <div className="App" id="main">
       // Do not alter the main div
-      <p id="para" className="hide">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <p id="para" className="hide show">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
       <button id="click" onClick={clickHandler}>Click me</button>
     </div>
   );
